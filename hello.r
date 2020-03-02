0613407de472608ccb622159f8112026bea99978
devincowan <dcquickdraw@gmail.com>
2020-03-02 11:40:00 -0700

    create new files every time

--- hello.r
+++ hello.r
@@ -1,5 +1,6 @@
 sayHello <- function(){
-   write(paste("hello, it is ",Sys.time()), "r_results/test.txt", append=TRUE)
+   # write(paste("hello, it is ",Sys.time()), "r_results/test.txt", append=TRUE)
+  write(paste("hello, it is ",Sys.time()), paste("r_results/","Sys.time()",".txt"), append=TRUE)
 }
 
 sayHello()
